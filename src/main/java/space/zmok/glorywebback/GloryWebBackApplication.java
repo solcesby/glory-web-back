921c5129e18435e06adc7f77ad21699a09d558a0
Mikhail.Kniga <solcesby@gmail.com>
2022-07-20 20:28:53 +0300

    Init commit

---
+++ src/main/java/space/zmok/glorywebback/GloryWebBackApplication.java
@@ -0,0 +1,13 @@
+package space.zmok.glorywebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloryWebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloryWebBackApplication.class, args);
+    }
+
+}
